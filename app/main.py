795c10bdcdc97d74669b5be59c2485f749b458e6
Baiyuan Qiu <1061688677@qq.com>
2023-01-04 16:09:22 +0800

    <your_commit_message>

--- app/main.py
+++ app/main.py
@@ -4,10 +4,8 @@ import socket
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-    print(addr)
-    conn.recv(1024)
-    conn.send(b"+PONG\r\n")
-    conn.close()
+    while conn.recv(1024):
+        conn.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
